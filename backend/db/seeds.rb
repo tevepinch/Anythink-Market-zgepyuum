781242b0131b864ec712d5e7bb364e9fe2c5eef7
tevepinch <96075932+tevepinch@users.noreply.github.com>
2023-10-25 01:45:24 +0000

    add seeds file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do |i|
+    User.create(
+      email: "user-#{i + 1}@example.com",
+      password: "password",
+      password_confirmation: "password",
+    )
+end
+
+100.times do |i|
+    Comment.create(
+        body: "comment",
+        user_id: 1,
+        item_id: 1,
+    )
+end
+
+100.times do |i|
+    Item.create(
+        title: "title",
+        slug: "slug",
+        description: "desc",
+        image: "image",
+        favorites_count: 1,
+        user_id: 1,
+    )
+end
+
+
